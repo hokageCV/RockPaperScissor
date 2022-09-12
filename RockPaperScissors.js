6192f6af868df08ec646ec3b5927c9ad51c15d6e
hokageCV <chaitanyavaru@gmail.com>
2022-09-12 22:34:52 +0530

    change colors

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -18,41 +18,44 @@ function playRound (playerSelection, computerSelection) {
 
     if(computerSelection == 'rock'){
         if(playerSelection == 'paper'){
-            console.log( `${playerSelection} vs ${computerSelection} : player jeet gaya ` );
+            round.textContent= `${playerSelection} vs ${computerSelection} : player jeet gaya ` ;
             playerScore++;   
         }
         else if( playerSelection == 'rock'){
-            console.log( `${playerSelection} vs ${computerSelection} : mukabala barabari ka raha ` );
+            round.textContent=  `${playerSelection} vs ${computerSelection} : mukabala barabari ka raha ` ;
         }
         else{
-            console.log( `${playerSelection} vs ${computerSelection} : computer jeet gaya ` );
+            round.textContent=  `${playerSelection} vs ${computerSelection} : computer jeet gaya ` ;
             computerScore++;
         }
     }
     else if(computerSelection == 'paper'){
         if(playerSelection == 'scissor'){
-            console.log( `${playerSelection} vs ${computerSelection} : player jeet gaya ` );
+            round.textContent=  `${playerSelection} vs ${computerSelection} : player jeet gaya ` ;
             playerScore++;
         }
         else if( playerSelection == 'paper')
-            console.log( `${playerSelection} vs ${computerSelection} : mukabala barabari ka raha ` );
+        round.textContent=  `${playerSelection} vs ${computerSelection} : mukabala barabari ka raha ` ;
         else{
-            console.log( `${playerSelection} vs ${computerSelection} : computer jeet gaya ` );
+            round.textContent=  `${playerSelection} vs ${computerSelection} : computer jeet gaya ` ;
             computerScore++;
         }
     }
     else if(computerSelection == 'scissor'){
         if(playerSelection == 'rock'){
-            console.log( `${playerSelection} vs ${computerSelection} : player jeet gaya ` );
+            round.textContent=  `${playerSelection} vs ${computerSelection} : player jeet gaya ` ;
             playerScore++;
         }
         else if( playerSelection == 'scissor')
-            console.log( `${playerSelection} vs ${computerSelection} : mukabala barabari ka raha ` );
+        round.textContent=  `${playerSelection} vs ${computerSelection} : mukabala barabari ka raha ` ;
         else{
-            console.log( `${playerSelection} vs ${computerSelection} : computer jeet gaya ` );
+            round.textContent=  `${playerSelection} vs ${computerSelection} : computer jeet gaya ` ;
             computerScore++;
         }
     }
+
+    console.log(round.textContent)
+    roundsDiv.appendChild(round)
 }
 
 
